9659bb1ee1006056671c0b4ee06daaa9063664ab
Jared Pines <pinesjared@gmail.com>
2023-02-01 16:56:52 -0800

    later

--- camera.js
+++ camera.js
@@ -8,8 +8,8 @@ class Camera {
     };
     cameraUp(){
         if(this.slideU < 997){
-            this.slideU += 1500*this.game.clockTick;
-            this.game.ctx.translate(0,1500*this.game.clockTick)
+            this.slideU += (2*997)*this.game.clockTick;
+            this.game.ctx.translate(0, (2*997)*this.game.clockTick)
         }else{
             let diff = this.slideU - 997
             this.game.ctx.translate(0,-diff)
@@ -19,8 +19,8 @@ class Camera {
     }
     cameraDown(){
         if(this.slideD > 0){
-            this.slideD -= 1500*this.game.clockTick;
-            this.game.ctx.translate(0,-1500*this.game.clockTick)
+            this.slideD -= (2*997)*this.game.clockTick;
+            this.game.ctx.translate(0,(2*-997)*this.game.clockTick)
         }else{
             let diff = this.slideD
             this.game.ctx.translate(0,-diff)
@@ -29,8 +29,8 @@ class Camera {
     }
     cameraLeft(){
         if(this.slide < 1471){
-            this.slide += 1500*this.game.clockTick;
-            this.game.ctx.translate(1500*this.game.clockTick,0)
+            this.slide += (2*1471)*this.game.clockTick;
+            this.game.ctx.translate((2*1471)*this.game.clockTick,0)
         }else{
             let diff = this.slide - 1471
             this.game.ctx.translate(-diff,0)
@@ -39,8 +39,8 @@ class Camera {
     }
     cameraRight(){
         if(this.slideR > 0){
-            this.slideR -= 1500*this.game.clockTick;
-            this.game.ctx.translate(-1500*this.game.clockTick,0)
+            this.slideR -= (2*1471)*this.game.clockTick;
+            this.game.ctx.translate((2*-1471)*this.game.clockTick,0)
         }else{
             let diff = this.slideR
             this.game.ctx.translate(-diff,0)
