12951f394a608de9dcd9064e5112f90871349ead
Jared Pines <pinesjared@gmail.com>
2023-03-12 20:23:56 -0700

    fix

--- premade_rooms.js
+++ premade_rooms.js
@@ -7,7 +7,6 @@ class Premade_Rooms {
         this.enemies = Array.from({length: 11}, () => new Array(6));
         this.floor = floor;
         this.r = Math.floor(Math.random() * 41)+1;
-        this.r = 40
         this.isaac = isaac;
         this.roomN = 0;
     };
